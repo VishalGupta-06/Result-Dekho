62873d4f00d151cf73d4038bafc2caec0a102b58
VishalGupta-06 <2024ugpi056@nitjsr.ac.in>
2026-07-05 20:29:47 +0530

    Update vite.config.js

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() ,  tailwindcss()],
+
+  build: {
+    sourcemap: true,
+  },
   server:{
     // proxy:{
     //   "/api":"http://localhost:8000"
