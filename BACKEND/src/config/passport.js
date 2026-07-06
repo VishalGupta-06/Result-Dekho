e3bc52bfe704cf8bbbfcc159f0fb8eb34a1c7228
VishalGupta-06 <2024ugpi056@nitjsr.ac.in>
2026-07-06 22:34:09 +0530

    Delete BACKEND/src directory

--- BACKEND/src/config/passport.js
+++
@@ -1,4 +0,0 @@
-import passport from "passport";
-import { googleLogin } from "../middleware/googleLogin.middleware.js";
-
-googleLogin(passport)
--- BACKEND/src/util/asyncHandler.js
+++
@@ -1,7 +0,0 @@
-const asyncHandler = (requestHandle) => {
-    return (req, res, next) => {
-        Promise.resolve(requestHandle(req, res, next)).catch((err) => next(err))
-
-    }
-}
-export { asyncHandler }
