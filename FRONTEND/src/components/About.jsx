f4c9b7778e54b94b9a1a6ee6bb9d40a7675c06cc
VishalGupta-06 <2024ugpi056@nitjsr.ac.in>
2026-07-08 17:46:52 +0530

    Update About.jsx

--- FRONTEND/src/components/About.jsx
+++ FRONTEND/src/components/About.jsx
@@ -115,17 +115,19 @@ function About() {
             <div className="mt-4 grid gap-3 sm:grid-cols-2">
               <div className="rounded-lg bg-slate-50 p-4">
                 <p className="font-bold text-slate-950">Vishal Kumar</p>
+                <p className="font-bold text-slate-550">2024UGPI056</p>
                 <p className="mt-1 text-sm text-slate-600">Admin</p>
               </div>
               
               <div className="rounded-lg bg-slate-50 p-4">
-                <p className="font-bold text-slate-950">Akriti Burnwal</p>
+                <p className="font-bold text-slate-950">Akriti Burnwal & Shikha Verma</p>
+                <p className="font-bold text-slate-550"> 2024UGPI058 & 2024YGPI057 </p>
                 <p className="mt-1 text-sm text-slate-600">Data Manage</p>
               </div>
-              <div className="rounded-lg bg-slate-50 p-4">
+              {/* <div className="rounded-lg bg-slate-50 p-4">
                 <p className="font-bold text-slate-950">Goldy</p>
                 <p className="mt-1 text-sm text-slate-600">Problem Creater</p>
-              </div>
+              </div> */}
             </div>
           </div>
         </section>
